d01bb5c41e3623ea7ddc6588fd6c554bf7ee0c2c
LC <2200724029@qq.com>
2020-11-11 14:43:29 +0800

    新功能

--- classic_menu.py
+++ classic_menu.py
@@ -3,35 +3,21 @@ import pygame,time
 from constant import *
 import music
 import loading
-from classic_menu import classic
 
-'''
-冒险模式
-    地图
-经典模式
-    训练模式
-    单人模式
-    双人模式
-设置
-关于 
-退出
-'''
 BG_COLOR = pygame.Color(0, 0, 0)
-class menu:
+class classic:
     window=None
     option=0
-    image_menu_maoxian = pygame.image.load('img/menu_maoxian.gif')
-    image_menu_jingdian = pygame.image.load('img/menu_jingdian.gif')
-    image_menu_shezhi = pygame.image.load('img/menu_shezhi.gif')
-    image_menu_guanyu = pygame.image.load('img/menu_guanyu.gif')
-    image_menu_tuichu = pygame.image.load('img/menu_tuichu.gif')
-    image_menu_exit = pygame.image.load('img/menu_exit.gif')
+    image_menu_xunlian = pygame.image.load('img/menu_xunlian.gif')
+    image_menu_danren = pygame.image.load('img/menu_danren.gif')
+    image_menu_shuangren = pygame.image.load('img/menu_shuangren.gif')
+    image_menu_fanhui = pygame.image.load('img/menu_fanhui.gif')
     def __init__(self):
         pass
     def openmenu(self):
         pygame.display.init()
         # 设置窗口的大小及显示
-        menu.window = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        classic.window = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
         # 设置窗口的标题
         pygame.display.set_caption('Soul Tank')
 
@@ -39,10 +25,12 @@ class menu:
         while True:
             #刷新频率
             time.sleep(0.02)
-            menu.window.fill(BG_COLOR)
-            menu.window.blit(image_main_background, (0, 0))
+            classic.window.fill(BG_COLOR)
+            classic.window.blit(image_main_background, (0, 0))
             # 获取事件
-            self.getEvent()
+            flag=self.getEvent()
+            if flag==0:
+                return
             pygame.display.update()
     def getEvent(self):
         # 获取所有事件
@@ -58,22 +46,23 @@ class menu:
                 # esc键切换全屏和窗口
                 if event.key == pygame.K_RETURN:
                     music.Music('img/fire2.wav')
-                    if self.option==1:
-                        classic().openmenu()
-                    if self.option==4:
-                        menu.window.blit(self.image_menu_exit, (0,0))
+                    if self.option==0:
+                        loading.load()
+                        MainGame().startGame()
+                    if self.option==3:
+                        return 0
                 elif event.key == pygame.K_s:
                     music.Music('img/choose.mp3')
                     self.option+=1
-                    if self.option>4:
+                    if self.option>3:
                         self.option=0
                 elif event.key == pygame.K_w:
                     music.Music('img/choose.mp3')
                     self.option-=1
                     if self.option<0:
-                        self.option=4
+                        self.option=3
                 print(self.option)
-        o1,o2,o3,o4,o5=0,0,0,0,0
+        o1,o2,o3,o4=0,0,0,0
         if self.option==0:
             o1=100
         elif self.option==1:
@@ -82,14 +71,7 @@ class menu:
             o3=100
         elif self.option==3:
             o4=100
-        elif self.option==4:
-            o5=100
-        menu.window.blit(self.image_menu_maoxian, (900-o1, 20))
-        menu.window.blit(self.image_menu_jingdian, (900-o2, 150))
-        menu.window.blit(self.image_menu_shezhi, (900-o3, 280))
-        menu.window.blit(self.image_menu_guanyu, (900-o4, 410))
-        menu.window.blit(self.image_menu_tuichu, (900-o5, 540))
-
-if __name__ == '__main__':
-    # zzz yyds
-    menu().openmenu()
+        classic.window.blit(self.image_menu_xunlian, (900-o1, 20))
+        classic.window.blit(self.image_menu_danren, (900-o2, 150))
+        classic.window.blit(self.image_menu_shuangren, (900-o3, 280))
+        classic.window.blit(self.image_menu_fanhui, (900-o4, 410))
