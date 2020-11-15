1df737d6166b69ef5d80bdffa244d35081fc5f18
LC <2200724029@qq.com>
2020-11-15 18:10:43 +0800

    新功能：单人游戏界面

--- single_play_menu.py
+++ single_play_menu.py
@@ -3,18 +3,15 @@ import pygame, time
 from constant import *
 import music
 import loading
-from single_play_menu import singleplay
 
 BG_COLOR = pygame.Color(0, 0, 0)
 
 
-class classic:
+class singleplay:
     window = None
     option = 0
-    image_menu_xunlian = pygame.image.load('img/menu_xunlian.gif')
-    image_menu_danren = pygame.image.load('img/menu_danren.gif')
-    image_menu_shuangren = pygame.image.load('img/menu_shuangren.gif')
-    image_menu_fanhui = pygame.image.load('img/menu_fanhui.gif')
+    image_menu = pygame.image.load('img/singleplay.gif')
+    image_select=pygame.image.load('img/select.gif')
 
     def __init__(self):
         pass
@@ -22,16 +19,14 @@ class classic:
     def openmenu(self):
         pygame.display.init()
         # 设置窗口的大小及显示
-        classic.window = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        singleplay.window = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
         # 设置窗口的标题
         pygame.display.set_caption('Soul Tank')
-
-        image_main_background = pygame.image.load('img/main_background.gif')
         while True:
             # 刷新频率
             time.sleep(0.02)
-            classic.window.fill(BG_COLOR)
-            classic.window.blit(image_main_background, (0, 0))
+            singleplay.window.fill(BG_COLOR)
+            singleplay.window.blit(self.image_menu, (0, 0))
             # 获取事件
             flag = self.getEvent()
             if flag == 0:
@@ -52,34 +47,32 @@ class classic:
                 # esc键切换全屏和窗口
                 if event.key == pygame.K_RETURN:
                     music.Music('img/fire2.wav')
-                    if self.option == 0:
+                    if self.option == 1:
                         loading.load()
                         MainGame().startGame()
-                    if self.option == 1:
-                        singleplay().openmenu()
-                    if self.option == 3:
+                    if self.option == 0:
                         return 0
                 elif event.key == pygame.K_s:
                     music.Music('img/choose.mp3')
-                    self.option += 1
-                    if self.option > 3:
-                        self.option = 0
+                    self.option += 5
+                    if self.option > 24:
+                        self.option = self.option -24-1
                 elif event.key == pygame.K_w:
+                    music.Music('img/choose.mp3')
+                    self.option -= 5
+                    if self.option < 0:
+                        self.option = 24+self.option+1
+                elif event.key == pygame.K_a:
                     music.Music('img/choose.mp3')
                     self.option -= 1
                     if self.option < 0:
-                        self.option = 3
+                        self.option = 24
+                elif event.key == pygame.K_d:
+                    music.Music('img/choose.mp3')
+                    self.option += 1
+                    if self.option >24:
+                        self.option = 0
                 print(self.option)
-        o1, o2, o3, o4 = 0, 0, 0, 0
-        if self.option == 0:
-            o1 = 100
-        elif self.option == 1:
-            o2 = 100
-        elif self.option == 2:
-            o3 = 100
-        elif self.option == 3:
-            o4 = 100
-        classic.window.blit(self.image_menu_xunlian, (900 - o1, 20))
-        classic.window.blit(self.image_menu_danren, (900 - o2, 150))
-        classic.window.blit(self.image_menu_shuangren, (900 - o3, 280))
-        classic.window.blit(self.image_menu_fanhui, (900 - o4, 410))
+        o=self.option//5
+        p=self.option%5
+        singleplay.window.blit(self.image_select, (180+p*180 , 110+o*100))
