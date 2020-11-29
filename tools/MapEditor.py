4e2502eeb9699f2c4af2607daf40fa40f158323b
zzzgod <845538195@qq.com>
2020-11-29 16:51:25 +0800

    尝试做地图编辑器

--- tools/MapEditor.py
+++ tools/MapEditor.py
@@ -1,38 +1,41 @@
-import pygame
-import json
+import tkinter as tk
+
+strings = []
 
 
-def show_block(block: dict) -> None:
-    img = None
-    if block['BlockType'] == 'Wall':
-        img = pygame.image.load('../img/walls.gif')
-    elif block['BlockType'] == 'Grass':
-        img = pygame.image.load('../img/grass.gif')
-    elif block['BlockType'] == 'Steel':
-        img = pygame.image.load('../img/steels.gif')
-    elif block['BlockType'] == 'Water':
-        img = pygame.image.load('../img/water.gif')
-    if img is not None:
-        window.blit(img, (block['x'] * 60, block['y'] * 60))
-
-
-def load_map(index: int) -> None:
-    path = '../maps/map' + str(index) + '.json'
-    with open(path, 'r') as f:
-        info: dict = json.load(f)
-        for block in info['MapBlocks']:
-            show_block(block)
+def set_img(i, j, choice):
+    def f():
+        strings[i][j].set(choice.get())
+    return f
 
 
 if __name__ == '__main__':
-    window = pygame.display.set_mode((1280, 720))
-    pygame.display.set_caption('地图编辑器')
-    load_map(1)
-    img_imformation = pygame.image.load('imformation.gif')
-    while True:
-        window.blit(img_imformation, (1140, 0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit(0)
-        pygame.time.wait(10)
-        pygame.display.update()
+    # 初始化窗口
+    window = tk.Tk()
+    # 标题
+    window.title('地图编辑器')
+    # 设置窗口大小
+    window.geometry('1280x720')
+    # 左侧工具栏
+    frame1 = tk.Frame(window)
+    # 生成提示文本
+    text = tk.Label(frame1, text='道具栏', width=20, height=2, font=('songti', 18)).pack(side='top')
+    frame1.pack(side='left')
+    # 记录选择
+    choice = tk.StringVar()
+    choice.set('0')
+    # 生成一堆按钮
+    for i in range(1, 10):
+        check_button = tk.Checkbutton(frame1, text=str(i), variable=choice, onvalue=str(i), offvalue='0', width=25, height=2,
+                                      bg='green', pady=10).pack()
+    # 右侧显示地图
+    frame2 = tk.Frame(window, pady=5, padx=10)
+    for i in range(12):
+        strings.append([])
+        for j in range(19):
+            s = tk.StringVar()
+            label = tk.Button(frame2, textvariable=s, width=6, height=2, bg='green', command=set_img(i, j, choice)).grid(row=i, column=j)
+            strings[-1].append(s)
+    frame2.pack(side='right')
+    # 窗口主循环
+    window.mainloop()
